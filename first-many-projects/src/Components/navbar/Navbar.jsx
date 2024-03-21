7e0f7294a00955a562b811ff7dfde99b6fc87683
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-21 19:48:45 +0100

    .

--- first-many-projects/src/Components/navbar/Navbar.jsx
+++ first-many-projects/src/Components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            NavBar
+        <div className={'gpt3-navbar'}>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links__logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+            </div>
         </div>
     )
 }
+
 
 export default Navbar
