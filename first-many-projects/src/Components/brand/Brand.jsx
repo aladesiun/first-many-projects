f386f0739abe0040d1dda5cf6a6c3ab13897f9cf
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-22 14:07:23 +0100

    .

--- first-many-projects/src/Components/brand/Brand.jsx
+++ first-many-projects/src/Components/brand/Brand.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './brand.css';
+import {atlassian, dropbox, google, shopify, slack} from "./import.js";
 
 const Brand = () => {
     return (
-        <div>
-            
+        <div className={'gpt3__brand section__padding'}>
+            <div>
+                <img src={google} alt="google"/>
+                <img src={slack} alt="slack"/>
+                <img src={atlassian} alt="atlassian"/>
+                <img src={dropbox} alt="dropbox"/>
+                <img src={shopify} alt="shopify"/>
+            </div>
         </div>
     )
 }
