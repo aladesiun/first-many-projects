a5b2b7b4497d5f8f40e0ea5e51ea678b8e935a86
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-22 10:44:23 +0100

    .

--- first-many-projects/src/Components/navbar/Navbar.jsx
+++ first-many-projects/src/Components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <div className="gpt3__navbar-links__logo">
                     <img src={logo} alt="logo"/>
                 </div>
-                <div className="gpt3__navbar-links__container">
+                <div className="gpt3__navbar-links_container">
                     <Menu />
                 </div>
             </div>
