b0c09bfceb2b5d8eda37ecc1062785d55d274fe2
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-25 20:57:26 +0100

    .

--- first-many-projects/src/Components/feature/Feature.jsx
+++ first-many-projects/src/Components/feature/Feature.jsx
@@ -3,8 +3,11 @@ import './feature.css';
 
 const Feature = () => {
     return (
-        <div>
-            Feature
+        <div className={'gpt3__features-container__feature'}>
+            <div className="gpt3__features-container__feature-title">
+                <div />
+                <h1>{title}</h1>
+            </div>
         </div>
     )
 }
