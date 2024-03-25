446a13bfb64bd478bf94e643f9463675429001b2
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-25 19:36:21 +0100

    .

--- first-many-projects/src/containers/whatGPT3/whatGPT3.jsx
+++ first-many-projects/src/containers/whatGPT3/whatGPT3.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import './whatGPT3.css';
+import Feature from "../../Components/feature/Feature.jsx";
 
 const whatGPT3 = () => {
     return (
-        <div>
-            whatGPT3
+        <div className={'gpt3__whatgpt3 section__padding'} id={'wgpt3'}>
+            <div className="gpt3__whatgpt3-feature">
+                <Feature />
+            </div>
+            <div className="gpt3__whatgpt3-heading">
+                <h1 className={'gradient__text'}>
+                    The possibilities are beyond your imagination
+                </h1>
+            </div>
         </div>
     )
 }
