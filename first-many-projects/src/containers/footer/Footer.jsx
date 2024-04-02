284850acc3b9b9c047fe8f5443270d0de9af6583
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-04-02 10:55:05 +0100

    .

--- first-many-projects/src/containers/footer/Footer.jsx
+++ first-many-projects/src/containers/footer/Footer.jsx
@@ -5,7 +5,7 @@ import gpt3Logo from '../../assets/logo.svg'
 const Footer = () => {
     return (
         <div className={'gpt3__footer section__padding'}>
-            <div className="gpt3__foter-heading">
+            <div className="gpt3__footer-heading">
                 <h1 className="gradient__text">Do you want to step in to the future before others</h1>
             </div>
             <div className="gpt3__footer-btn">
@@ -36,7 +36,7 @@ const Footer = () => {
                     <p>info@payme.net</p>
                 </div>
             </div>
-            <div className="gpt__footer-copyright">
+            <div className="gpt3__footer-copyright">
                 <p>© 2021 GPT-3. All rights reserved.</p>
             </div>
         </div>
